76e312b40c76cb187c96928f266d4d8bf9060b78
wuzhdcm <1360520870@qq.com>
2023-05-09 10:52:14 +0800

    third

--- src/main/java/com/hnkjxy/GitTest.java
+++ src/main/java/com/hnkjxy/GitTest.java
@@ -2,20 +2,14 @@ package com.hnkjxy;
 
 /**
  * @author wuzihao
-<<<<<<< HEAD
  * @date 2023/5/9 - 10:45
-=======
- * @date 2023/5/9 - 10:38
->>>>>>> dev
  */
 public class GitTest {
 
   public static void main(String[] args) {
-<<<<<<< HEAD
     System.out.println("22222");
-=======
     System.out.println("111111");
->>>>>>> dev
+    System.out.println("3333333");
   }
 
 }
