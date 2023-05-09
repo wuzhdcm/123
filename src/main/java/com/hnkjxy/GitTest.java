75d4de6800973958407e209d5e8436f269c85e08
wuzhdcm <1360520870@qq.com>
2023-05-09 10:45:24 +0800

    second

---
+++ src/main/java/com/hnkjxy/GitTest.java
@@ -0,0 +1,13 @@
+package com.hnkjxy;
+
+/**
+ * @author wuzihao
+ * @date 2023/5/9 - 10:45
+ */
+public class GitTest {
+
+  public static void main(String[] args) {
+    System.out.println("22222");
+  }
+
+}
